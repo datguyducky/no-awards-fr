fdf69be2081c9545109ebdd9ca915b3718a45f91
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2021-10-23 01:23:30 +0200

    working settings menu for all the options, seperate styling to different css files

--- handle-settings.js
+++ handle-settings.js
@@ -1,58 +1,59 @@
 const saveSettings = () => {
-  // get all select elements
-  const GIVE_AWARD_BTNS = document.getElementById("toggle-op1").checked;
-  const RPAN_AWARDS = document.getElementById("toggle-op2").checked;
-  const COMMENTS_AWARDS = document.getElementById("toggle-op3").checked;
-  const POSTS_AWARDS = document.getElementById("toggle-op4").checked;
-  const GET_COINS_BTN = document.getElementById("toggle-op5").checked;
+	// get all select elements
+	const GIVE_AWARD_BTNS = document.getElementById("toggle-op1").checked;
+	const RPAN_AWARDS = document.getElementById("toggle-op2").checked;
+	const COMMENTS_AND_POSTS_AWARDS =
+		document.getElementById("toggle-op3").checked;
+	const GET_COINS_BTN = document.getElementById("toggle-op4").checked;
 
-  // save select elements :checked value to browser local storage
-  chrome.storage.local.set(
-    {
-      giveAwardsBtn: GIVE_AWARD_BTNS,
-      rpanAwards: RPAN_AWARDS,
-      commentsAwards: COMMENTS_AWARDS,
-      postsAwards: POSTS_AWARDS,
-      getCoinsBtn: GET_COINS_BTN,
-    },
-    () => {
-      setTimeout(() => {
-        // display the tooltip element (after 450ms) to let user know that settings were successfully saved
-        const TOOLTIP = document.getElementsByClassName("tooltiptext")[0];
-        TOOLTIP.style.visibility = "visible";
-        TOOLTIP.style.opacity = 1;
+	// save select elements :checked value to browser local storage
+	chrome.storage.local.set(
+		{
+			giveAwardsBtn: GIVE_AWARD_BTNS,
+			rpanAwards: RPAN_AWARDS,
+			commentsAndPostsAwards: COMMENTS_AND_POSTS_AWARDS,
+			getCoinsBtn: GET_COINS_BTN,
+		},
+		() => {
+			setTimeout(() => {
+				// display the tooltip element (after 450ms) to let user know that settings were successfully saved
+				const TOOLTIP =
+					document.getElementsByClassName("tooltiptext")[0];
+				TOOLTIP.style.visibility = "visible";
+				TOOLTIP.style.opacity = 1;
 
-        // hide the tooltip after 1300ms
-        setTimeout(() => {
-          TOOLTIP.style.visibility = "hidden";
-          TOOLTIP.style.opacity = 0;
-        }, 1300);
-      }, 450);
-    }
-  );
+				// hide the tooltip after 1300ms
+				setTimeout(() => {
+					TOOLTIP.style.visibility = "hidden";
+					TOOLTIP.style.opacity = 0;
+				}, 1300);
+			}, 450);
+		}
+	);
 };
 
 // load all the select elements :checked value and assign it to the elements itself
 const loadSettings = () => {
-  chrome.storage.local.get(
-    {
-      giveAwardsBtn: false,
-      rpanAwards: true,
-      commentsAwards: true,
-      postsAwards: true,
-      getCoinsBtn: false,
-    },
-    (settings) => {
-      document.getElementById("toggle-op1").checked = settings.giveAwardsBtn;
-      document.getElementById("toggle-op2").checked = settings.giveAwardsBtn;
-      document.getElementById("toggle-op3").checked = settings.commentsAwards;
-      document.getElementById("toggle-op4").checked = settings.postsAwards;
-      document.getElementById("toggle-op5").checked = settings.getCoinsBtn;
-    }
-  );
+	chrome.storage.local.get(
+		{
+			giveAwardsBtn: false,
+			rpanAwards: true,
+			commentsAndPostsAwards: true,
+			getCoinsBtn: false,
+		},
+		(settings) => {
+			document.getElementById("toggle-op1").checked =
+				settings.giveAwardsBtn;
+			document.getElementById("toggle-op2").checked = settings.rpanAwards;
+			document.getElementById("toggle-op3").checked =
+				settings.commentsAndPostsAwards;
+			document.getElementById("toggle-op4").checked =
+				settings.getCoinsBtn;
+		}
+	);
 };
 
 document.addEventListener("DOMContentLoaded", loadSettings);
 document
-  .getElementsByClassName("save-btn")[0]
-  .addEventListener("click", saveSettings);
+	.getElementsByClassName("save-btn")[0]
+	.addEventListener("click", saveSettings);
--- no-awards.js
+++ no-awards.js
@@ -1,10 +1,20 @@
-const loadStyling = () => {
-  const styling = document.createElement("link");
-  styling.rel = "stylesheet";
-  styling.type = "text/css";
-  styling.href = chrome.runtime.getURL("no-awards.css");
+const applyStyling = (key) => {
+	const styling = document.createElement("link");
+	styling.rel = "stylesheet";
+	styling.type = "text/css";
+	styling.href = chrome.runtime.getURL(`css/${key}.css`);
 
-  document.documentElement.insertBefore(styling, null);
+	document.documentElement.insertBefore(styling, null);
+};
+
+const loadStyling = () => {
+	chrome.storage.local.get(null, (userSettings) => {
+		Object.entries(userSettings).forEach(([key, checked]) => {
+			if (checked) {
+				applyStyling(key);
+			}
+		});
+	});
 };
 
 loadStyling();
